26019ad9e87dcefd3ece98fad6cd17414b2cecb6
Dilip Kumar <dilip.sharma4558@gmail.com>
2026-09-22 14:36:23 +0530

    Selection Sort

---
+++ SelectionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            int minIdx=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[minIdx]){
+                    minIdx=j;
+                }
+            }
+            
+            if(minIdx!=i){
+                int temp=arr[i];
+                arr[i]=arr[minIdx];
+                arr[minIdx]=temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
